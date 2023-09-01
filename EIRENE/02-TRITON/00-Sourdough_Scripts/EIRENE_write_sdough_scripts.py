348d22fd9b834018eaaf9c8540b81f544b64fa2a
C. Erika Moss <84610699+cerikam@users.noreply.github.com>
2023-09-01 09:13:34 -0400

    EIRENE_write_sdough_scripts.py

---
+++ EIRENE/02-TRITON/00-Sourdough_Scripts/EIRENE_write_sdough_scripts.py
@@ -0,0 +1,338 @@
+# *************************************************************************************************
+#
+#         Working script for writing multiple directories and EIRENE.inp files for
+#         different refuel rates.
+#
+# *************************************************************************************************
+
+import sys, re
+import os.path
+import argparse
+import subprocess
+import salts_wf
+import math
+import numpy as np
+import EIRENE_sourdough2 as es
+
+refuel_enrichment = 0.03            # Refuel enrichment fraction (0.03 = 3% enriched U235)
+dep_step = 7                        # Length of single depletion step (days)
+UF4molpct = 5.0                     # UF4 molar percent
+rrates = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]     # Define refuel rates here in units of cm3/hour
+
+for x in range(len(rrates)):
+    scale_fuel = es.write_scale_mat("EIRENE_TRITON_1step.f71", rrates[x], dep_step, refuel_enrichment)
+    He_cyl_bottom = es.add_refuel_to_core(rrates[x], dep_step)
+    deckpath = f'rrate_{rrates[x]:5.01f}'
+    if not os.path.isdir(deckpath):
+        os.mkdir(deckpath)
+    os.chdir(deckpath)
+
+    keno_deck = f'''=csas6 parm=(   )
+EIRENE SCALE/CSAS model, UF4 mol% = {UF4molpct} and refuel enrichment {refuel_enrichment}
+ce_v7.1
+
+read comp
+' FLIBe-U fuel salt
+{scale_fuel}
+
+' Hastelloy N
+wtptHastelloy 2 8.89 5
+         28000 71.0
+         24000 7.0
+         26000 5.0
+         14000 1.0
+         42000 16.0
+         1.0 923.15
+         28058 67.6 28060 32.4
+         24052 100.0
+         26056 100.0
+         14028 100.0
+         42092 14.65 42094 9.19 42095 15.87 42096 16.67 42097 9.58 42098 24.29 42100 9.75 end
+' SS Shutdown Rods
+    ss316 3 den=2.7 1.0 923.15 end
+' Graphite
+   graphite 4 den=1.84 1.0 923.15 end
+' Stainless Steel SS316
+   ss316 5 den=8.030000 1.0 923.15 end
+' Helium gas
+   he 6 den=0.0001785 1.0 923.15 end
+
+end comp
+
+read parameters
+ npg=20000 nsk=50 gen=10050 sig=10e-5
+ htm=no
+ fdn=no
+ pms=no
+ pmv=no
+end parameters
+
+read geometry
+' ****************************************************************************
+'                             CONTROL RODS
+' ****************************************************************************
+
+' Verify control rod height
+
+' Unit 1: Control rod unit cell (cylinder 1 = shutdown rod; cylinder 2 = guide tube)
+unit 1
+' Shutdown rod (Gd)
+  cylinder 1 7.0 415.0 130
+' Guide tube
+  cylinder 2 7.2 415.0 130
+' Fuel region beneath
+  cylinder 3 7.2 130 0.0
+' Graphite hexprism
+  hexprism 4 13.076 415.0 0.0
+  media 3 1 1
+  media 2 1 -1 2
+  media 1 1 3
+  media 4 1 4 -1 -2 -3
+  boundary 4
+
+' ****************************************************************************
+'                        REGION 1 FUEL ASSEMBLIES
+' ****************************************************************************
+
+' ----- Small molten salt fuel channel -----
+
+' Unit 2: Region 1 Fuel Assembly - Single Cylindrical Unit
+unit 2
+' Single small fuel channel
+  cylinder 1 1.157 415.0 0.0
+' Graphite hexprism to contain fuel channel
+  hexprism 2 2.6 415.0 0.0
+  media 1 1 1
+  media 4 1 -1 2
+  boundary 2
+
+' Unit 3: Region 1 Fuel Assembly - Bare Graphite Hexprism
+unit 3
+  hexprism 1 2.6 415.0 0.0
+  media 4 1 1
+  boundary 1
+
+' Unit 4: Region 1 Fuel Assembly Hex
+unit 4
+' Cylinder to contain array of fuel channel elements
+  cylinder 1 12.5 415.0 0.0
+' Graphite hexprism
+  hexprism 2 13.076 415.0 0.0
+  array 1 1 place 5 5 1 0.0 0.0 0.0
+  media 4 1 2 -1
+  boundary 2
+
+' ****************************************************************************
+'                        REGION 2 FUEL ASSEMBLIES
+' ****************************************************************************
+
+' ----- Medium molten salt fuel channel -----
+
+' Unit 5: Region 2 Fuel Assembly - Single Cylindrical Unit
+unit 5
+' Single medium fuel channel
+  cylinder 1 1.2197 415.0 0.0
+' Graphite hexprism to contain fuel channel
+  hexprism 2 2.6 415.0 0.0
+  media 1 1 1
+  media 4 1 -1 2
+  boundary 2
+
+' Bare Graphite Hexprism is the same for this region as Region 1 (Unit 3)
+
+' Unit 6 - Region 2 Fuel Assembly Hex
+unit 6
+' Cylinder to contain array of fuel channel elements
+  cylinder 1 12.5 415.0 0.0
+' Graphite hexprism
+  hexprism 2 13.076 415.0 0.0
+  array 2 1 place 5 5 1 0.0 0.0 0.0
+  media 4 1 2 -1
+  boundary 2
+
+' ****************************************************************************
+'                        REGION 3 FUEL ASSEMBLIES
+' ****************************************************************************
+
+' ----- Large molten salt fuel channel -----
+
+' Unit 7: Region 3 Fuel Assembly - Single Cylindrical Unit
+unit 7
+' Single large fuel channel
+  cylinder 1 1.3485 415.0 0.0
+' Graphite hexprism to contain fuel channel
+  hexprism 2 2.6 415.0 0.0
+  media 1 1 1
+  media 4 1 -1 2
+  boundary 2
+
+' Bare Graphite Hexprism is the same for this region as Region 1 (Unit 3)
+
+' Unit 8 - Region 3 Fuel Assembly Hex
+unit 8
+' Cylinder to contain array of fuel channel elements
+  cylinder 1 12.5 415.0 0.0
+' Graphite hexprism
+  hexprism 2 13.076 415.0 0.0
+  array 3 1 place 5 5 1 0.0 0.0 0.0
+  media 4 1 2 -1
+  boundary 2
+
+' ****************************************************************************
+'                        GRAPHITE REFLECTOR CELLS
+' ****************************************************************************
+
+' Single hexprism of graphite
+unit 9
+  hexprism 1 13.076 415.0 0.0
+  media 4 1 1
+  boundary 1
+
+' ****************************************************************************
+'                              GLOBAL UNIT
+' ****************************************************************************
+
+global unit 10
+' Core Volume (fill with Array 4)
+  cylinder 1 190 415 0.0
+' Hastelloy N Core Blanket
+  cylinder 2 195 415 0.0
+' Downcomer Region
+  cylinder 3 200 440.5 -10.5
+' Hastelloy N Reactor Vessel
+  cylinder 4 205 565 -15.5
+' Reflector (top) (Hastelloy N)
+  cylinder 5 195 430 415
+' Outlet Plenum
+  cylinder 6 195 435.5 430
+' Inlet Plenum
+  cylinder 7 195 0.0 -5.5
+' Fuel Inside Helium Cylinder
+  cylinder 8 200 {He_cyl_bottom} 440.5
+' Helium Cylinder
+  cylinder 9 200 560 {He_cyl_bottom}
+' Main array placement
+  array 4 1 place 10 10 1 0.0 0.0 0.0
+  media 2 1 -1 2
+' Downcomer media
+  media 1 1 3 -2 -5 -6 -7 -8 -9
+' Reactor Vessel Media
+  media 2 1 4 -5 -3 -6 -7 -8 -9
+' Reflector (top) media
+  media 2 1 5
+' Outlet Plenum media
+  media 1 1 6
+' Inlet Plenum media
+  media 1 1 7
+' Fuel Inside Helium Cylinder Media
+  media 1 1 8
+' Helium Cylinder media
+  media 6 1 9
+  boundary 4
+end geometry
+
+read array
+' Array 1: Fuel Region 1 Single Hex
+  ara=1
+  prt=yes
+  typ=triangular
+  nux=9
+  nuy=9
+  nuz=1
+  fill
+' z = 1
+    3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3
+      3 3 3 3 2 2 2 3 3
+       3 3 3 2 2 2 2 3 3
+        3 3 2 2 2 2 2 3 3
+         3 3 2 2 2 2 3 3 3
+          3 3 2 2 2 3 3 3 3
+           3 3 3 3 3 3 3 3 3
+            3 3 3 3 3 3 3 3 3
+  end fill
+
+' Array 2: Fuel Region 2 Single Hex
+  ara=2
+  prt=yes
+  typ=triangular
+  nux=9
+  nuy=9
+  nuz=1
+  fill
+' z = 1
+    3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3
+      3 3 3 3 5 5 5 3 3
+       3 3 3 5 5 5 5 3 3
+        3 3 5 5 5 5 5 3 3
+         3 3 5 5 5 5 3 3 3
+          3 3 5 5 5 3 3 3 3
+           3 3 3 3 3 3 3 3 3
+            3 3 3 3 3 3 3 3 3
+  end fill
+
+' Array 3: Fuel Region 2 Single Hex
+  ara=3
+  prt=yes
+  typ=triangular
+  nux=9
+  nuy=9
+  nuz=1
+  fill
+' z = 1
+    3 3 3 3 3 3 3 3 3
+     3 3 3 3 3 3 3 3 3
+      3 3 3 3 7 7 7 3 3
+       3 3 3 7 7 7 7 3 3
+        3 3 7 7 7 7 7 3 3
+         3 3 7 7 7 7 3 3 3
+          3 3 7 7 7 3 3 3 3
+           3 3 3 3 3 3 3 3 3
+            3 3 3 3 3 3 3 3 3
+  end fill
+
+' Array 4: Main Array Containing All Hex Elements
+  ara=4
+  prt=yes
+  typ=triangular
+  nux=21
+  nuy=21
+  nuz=1
+  fill
+' z = 1
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+     9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+      9 9 9 9 9 9 9 9 9 9 9 9 8 8 9 9 9 9 9 9 9
+       9 9 9 9 9 9 9 9 9 8 8 8 8 8 8 8 9 9 9 9 9
+        9 9 9 9 9 9 9 9 8 6 6 6 6 6 6 8 9 9 9 9 9
+         9 9 9 9 9 9 8 8 6 6 6 6 6 6 6 8 9 9 9 9 9
+          9 9 9 9 9 8 8 6 6 6 6 6 6 6 6 8 8 9 9 9 9
+           9 9 9 9 9 8 6 6 6 4 1 4 6 6 6 8 8 9 9 9 9
+            9 9 9 9 8 6 6 6 1 4 4 1 6 6 6 8 9 9 9 9 9
+             9 9 9 8 6 6 6 4 4 4 4 4 6 6 6 8 9 9 9 9 9
+              9 9 9 8 6 6 6 1 4 4 1 6 6 6 8 9 9 9 9 9 9
+               9 9 9 8 6 6 6 4 1 4 6 6 6 8 9 9 9 9 9 9 9
+                9 9 8 8 6 6 6 6 6 6 6 6 8 8 9 9 9 9 9 9 9
+                 9 9 8 8 6 6 6 6 6 6 6 8 8 9 9 9 9 9 9 9 9
+                  9 9 9 8 6 6 6 6 6 6 8 9 9 9 9 9 9 9 9 9 9
+                   9 9 9 8 8 8 8 8 8 8 9 9 9 9 9 9 9 9 9 9 9
+                    9 9 9 9 9 8 8 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+                     9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+                      9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+                       9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+                        9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+  end fill
+end array
+
+end data
+end
+'''
+
+    fout = open("EIRENE.inp", "w")  # Dump deck into file
+    fout.write(keno_deck)
+    fout.close()
+
+#    os.system('qsub ../../../../util/runEIRENE-Scale.sh')  # Submit job - Uncomment to actually submit the Cluster jobs!
+
+    os.chdir('..')
