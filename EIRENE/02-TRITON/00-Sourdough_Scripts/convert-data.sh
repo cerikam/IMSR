3985293b26276cc5da3c328b26a53e2bb46a5ef8
C. Erika Moss <84610699+cerikam@users.noreply.github.com>
2024-03-26 20:43:11 -0400

    Removing outdated scripts to make way for revised ones

--- EIRENE/02-TRITON/00-Sourdough_Scripts/convert-data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-#
-
-grep 'best estimate' refuel_*/EIRENE.out | sed -E -e s/^refuel.//g -e 's/.EIRENE.*eff//g'  -e 's/\+ or \-//g' -e 's/\*\*\*//g' -e 's/\s+/ /g' | sort -g  > data-temp.out
